42ba1b90237e6f3fd842c7be9c5a46eff587ad35
Juan Fran Blanco <juanfranblanco@hotmail.com>
2016-03-23 07:59:21 +0000

    start abi

--- app.js
+++ app.js
@@ -6,6 +6,51 @@ var fs = require("fs");
 
 var returnContracts  = new Array();
 
+var generateInputParameters = function(signature, joinedParameterNames){
+    var parameterNames = joinedParameterNames.split(',');
+    var params = new Array();
+    var signatureArray = signature.split('');
+    for (var index = 0; index < parameterNames.length; index++) {
+        var parameterName = parameterNames[index].trim();
+        
+        if(parameterName.indexOf(':') > -1){
+            parameterName = parameterName.substring(0, parameterName.indexOf(':')).trim();
+        }
+        
+        var param = {};
+        param.name = parameterName;
+        param.signature = signatureArray[index];
+        switch (param.signature) {
+            case 'i':
+                 param.type = 'int';
+            break;
+            case 's':
+                param.type = 'string';
+            break;
+            case 'a':
+                param.type = 'bytes32[]'
+            break; 
+            default:
+                
+            break;
+        }
+        params.push(param);
+    }
+    return params;
+}
+
+var generateABIFunction = function(serpentFuction){
+    var func = {};
+    func.name = serpentFuction.functionName;
+    func.type = "function";
+    func.serpent = true;
+    func.constant = false;
+    if(typeof serpentFuction.parameters !== 'undefined' && serpentFuction.parameters != '' && typeof serpentFuction.signature !== 'undefined') {
+        func.input = generateInputParameters(serpentFuction.signature, serpentFuction.parameters);
+    }
+    return func;
+}
+
 Object.keys(tx).forEach(function(key){
     var txElement = tx[key];
     serpentContracts.forEach(function(contract) {
@@ -16,6 +61,10 @@ Object.keys(tx).forEach(function(key){
                func.returns = txElement.returns;
                contract.to = txElement.to;
                func.send = txElement.send;
+               
+                func.abi = generateABIFunction(func);
+               
+               
                if(returnContracts.indexOf(contract) === -1){
                    returnContracts.push(contract);
                }
@@ -23,6 +72,8 @@ Object.keys(tx).forEach(function(key){
        }, this);
     }, this);
 });
+
+
 
 fs.writeFileSync("contractsInfo.json", JSON.stringify(returnContracts, null, 4), 'utf8');
  
